44982c08db5e82e7e52783d6a19b16f14e3f1477
Katherine Misyutina <kmisyu2@uic.edu>
2019-10-12 21:29:37 -0500

    adding spring button

---
+++ Assets/ButtonTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    public Material[] materials;
+    //Renderer rend;
+    private bool lightOn = false;
+
+    public GameObject light1;
+    public GameObject light2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rend = GetComponent<Renderer>();
+        //rend.enabled = true;
+        //rend.sharedMaterial = materials[0];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "button")
+        {
+            if (lightOn == false)
+            {
+                lightOn = true;
+                //rend.sharedMaterial = materials[1];
+                light1.GetComponent<MeshRenderer>().material = materials[1];
+            }
+            else
+            {
+                lightOn = false;
+                //rend.sharedMaterial = materials[0];
+                light1.GetComponent<MeshRenderer>().material = materials[0];
+            }
+        }
+    }
+
+
+}
